e1f5692dd2a5b726a2f638d5929ed0f85753f788
buuuuuuuuuuuuuuuu <53521790+buuuuuuuuuuuuuuuu@users.noreply.github.com>
2026-07-11 13:20:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '1.6';
+const VERSION = '1.7';
 const CACHE = 'liste-v' + VERSION;
 
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
